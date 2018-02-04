142bc17feaedafbf5cfef485165367a9a2341fe6
Instructor <poatir@gmail.com>
2018-02-03 19:34:54 -0500

    B125 Posesión de AI en los otros tanques
    En el Tank_BP se debe establecer la propiedad AI Controller Class=TankAIController
    El resto deropiedades se deja igual.
    En TankAIController se declara el BeginPlay() y GetControlledTank()  como en TankPlayerController.h
    y en TankAIController.cpp se implementan estos dos metodos igual que en TankPlayerController.cpp.
    
    Debe imprimirse por log el nombre de cada uno de los tanques instanciados en la escena.
    Cada vez que usted agrege uno este tendra AI poseida automaticamente.

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tanque AI no poseido"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tanque AI poseido: %s"), *ControlledTank->GetName());
+	}
+}
+
+
