5bd1a94610da7423820d56f64d6f83639300b38b
Instructor <poatir@gmail.com>
2018-02-07 01:55:28 -0500

    134 Obteniendo la posición del Barrel (Cañon) componente del tanque
    Para poder crear una trayectoria del proyectil, es necesario saber de donde parte y a donde cae.
    Ya tenemos el HitLocation, es decir donde cae el proyectil, pero no sabemos de donde parte.  Es necesario saber la coordenada 3D en donde esta ubicada ese cañon.
    
    En primer lugar en la clase TankAimingComponent.h Declaramos lo siguiente:
    
    private:
    	UStaticMeshComponent* Barrel = nullptr;
    
    Luego creamos este metodo para obtener la referencia del componte Barrel que tiene cada uno de los tanques:
    
    void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
    {
    	Barrel = BarrelToSet;
    }
    
    Para poder exponer un metodo en Blueprints hecho con lenguaje c++ es necesario crear Una UFunction asi:
    
    UFUNCTION (BlueprintCallable, Category = Setup)
    void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
    
    Luego implementamos dicho metodo en Tank.cpp:
    
    void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
    {
    	TankAimingComponent->SetBarrelReference(BarrelToSet);
    }
    
    De esta forma se utiliza el metodo set  del componente TankAimingComponent y queda visible desde los blueprints
    
    Desde TankAimingComponent.cpp actualizamos el metodo AimAt para que imprima coordenada final HitLocation y la coordenada del Barrel es decir la coordenada inicial.
    
    void UTankAimingComponent::AimAt(FVector HitLocation)
    {
    	auto OurTankName = GetOwner()->GetName();
    	UE_LOG(LogTemp, Warning, TEXT("%s aimingAt: %s"), *OurTankName, *HitLocation.ToString());
    	auto BarrelLocation = Barrel->GetComponentLocation();
    	UE_LOG(LogTemp, Warning, TEXT("%s aimingAt: %s from Barrel: %s"), *OurTankName, *HitLocation.ToString(), *BarrelLocation.ToString());
    }
    
    Es necesario Compilar los cambios para que podamos hacer uso  del metodo SetBarrelReference para hacerlo visible desde los blueprints.
    
    una vez compilado. Entramos al Tank_BP a su Event Graph
    y arrastramos una instancia referencia del barrel, luego desde su pin de salida invocamos el nodo metodo llamado  Set Barrel Refeence y lo vinculamos al BeginPlay
    
    Al ejecutar debe imprimir las coordenas incial y final de cada tanque.
    Ojo: Debido a que se uso Cache por cada par del tanque, sus coordenadas relativas siempre son 0,0,0.
    Al imprimir la ubicacion de cada Barrel, se imprime su posición con respecto al mundo.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -11,6 +11,9 @@ class BATTLETANK_API ATank : public APawn
 	GENERATED_BODY()
 public:
 	void AimAt(FVector HitLocation);
+
+	UFUNCTION (BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
 private:
