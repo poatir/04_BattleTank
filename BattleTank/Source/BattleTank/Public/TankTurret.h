f2887196b34ca0fe19224801544f2fc2809eb419
poatir <poatir@gmail.com>
2018-02-26 17:22:42 -0500

    B146 Hacer girar el Turret parte 2
    Basado en la clase TankBarrel, se copia el metodo Elevate y se crea el metodo Rotate en TankTurret.
    Practicamente es lo mismo, solo que hay que cambiar Pitch por Yaw y relativeRotation (con sus respectivo parametros)
    
    void UTankTurret::Rotate(float RelativeSpeed)
    {
    	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
    	auto RotationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
    	auto Rotation = RelativeRotation.Yaw + RotationChange;
    	SetRelativeRotation(FRotator(0,Rotation,0));
    }
    
    Logicamente debe declarase en el archivo TankTurret.h el metodo rotate y la propiedad MaxDegreePersSecond=25.
    
    Esto define la velocidad de giro del turret.
    Debe compilarse el proyecto, para poder cambiar esta propiedad desde la grafica de eventos del blueprint.
    
    Para esto debe seleccionar el turret.
    
    En la clase TankAimingComponent.cpp debve modificar el metodo MoveBarrelTowards y escribir en la ultima linea:
    Turret->Rotate(DeltaRotator.Yaw);

--- BattleTank/Source/BattleTank/Public/TankTurret.h
+++ BattleTank/Source/BattleTank/Public/TankTurret.h
@@ -13,7 +13,12 @@ class BATTLETANK_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+public:
+
+	void Rotate(float RelativeSpeed);
+
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreesPerSecond = 25;
+
 };
