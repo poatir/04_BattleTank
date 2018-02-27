c6a6c1fbe63dda2409dd87c1850f342ca949afc7
poatir <poatir@gmail.com>
2018-02-26 23:12:51 -0500

    B150 Areglando los problemas de collision y temblores
    Corrección del colisión del tanque.
    
    Primero entramos al Blueprint de Tank_BP
    Abre por separado Tank_fbx_Track (malla estatica) que tiene asignado 2 objetos de colisión muy compleja.
    En la ventana de detalles (propiedades) deshabilitar "activar Collision" debe quedar la colisión mas sencilla visible.
    Luego en import settings deshabilitar "auto generate collision".
    
    Todas estas acciones atenuan un poco el problema de temblor de los tanques, pro es necesario tomar mas acciones
    
    Abrir por separado Tank_fbx_Turret
    En este caso sucede la misma situación hay dos objetos de collision.
    En la ventana de detalles (propiedades) deshabilitar "activar Collision" debe quedar la colisión mas sencilla visible.
    Luego en import settings deshabilitar "auto generate collision".
    
    Abrir por separado Tank_fbx_Barrel
    En este caso sucede la misma situación hay dos objetos de collision.
    En la ventana de detalles (propiedades) deshabilitar "activar Collision" debe quedar la colisión mas sencilla visible.
    Luego en import settings deshabilitar "auto generate collision".
    
    En la clase TankTurret.h se debe quitar la macro que esconde la sección , HideCategories = ("Collision")
    En la clase TankBarrel.h se debe quitar la macro que esconde la sección , HideCategories = ("Collision")
    
    Dentro del Blueprint del Tank_BP selecciona Left Track y Right Track (llantas del tanque) y Turret y Barrel deshabilita "Enable Gravity"
    
    Del componente Turret del Tank_BP en collision preset = NoCollision
    Del componente Barrel del Tank_BP en collision preset = NoCollision
    
    De Tank_fbx_Turret y Tank_fbx_Barrel Eliminar sus objetos de collision en el menu principal Collision->Remove Collision

--- BattleTank/Source/BattleTank/Public/TankTurret.h
+++ BattleTank/Source/BattleTank/Public/TankTurret.h
@@ -8,7 +8,7 @@
 /**
  * 
  */
-UCLASS(meta = (BlueprintSpawnableComponent), HideCategories = ("Collision"))
+UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
