eda56b218f49c12a1de0c361dc760d10388809e2
poatir <poatir@gmail.com>
2018-02-27 18:36:16 -0500

    B152 Disparando el proyectil
    En la clase Projectile.h declare como privado:
    
    UProjectileMovementComponent* ProjectileMovement = nullptr;
    En el contructor de la clase Projectile.cpp incluya:
    
    Nota: Esto crea un subobjeto utilizable desde los blueprints
    
    ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
    ProjectileMovement->bAutoActivate = false;//deiene la bala antes de disparar
    
    En projectile.h declare el siguiente metodo:
    
    void LaunchProjectile(float Speed);
    
    logicamente implementar Launchprojectile en el Projectile.cpp
    En Tank.cpp Modifique el metodo Fire asi:
    
    void ATank::Fire()
    {
    	//auto Time = GetWorld()->GetTimeSeconds();
    	//UE_LOG(LogTemp, Warning, TEXT("%f Tank Fires"), Time);
    
    	if (!Barrel) { return; }
    	auto Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileBlueprint,Barrel->GetSocketLocation(FName("Projectile")), Barrel->GetSocketRotation(FName("Projectile")));
    	Projectile->LaunchProjectile(LaunchSpeed);
    }
    
    La implementacion de LaunchProjectile:
    
    void AProjectile::LaunchProjectile(float Speed)
    {
    	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
    	ProjectileMovement->Activate();
    }

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -53,9 +53,9 @@ void ATank::SetTurretReference(UTankTurret* TurretToSet)
 }
 void ATank::Fire()
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f Tank Fires"), Time);
-
+	//auto Time = GetWorld()->GetTimeSeconds();
+	//UE_LOG(LogTemp, Warning, TEXT("%f Tank Fires"), Time);
 	if (!Barrel) { return; }
-	GetWorld()->SpawnActor<AProjectile>(ProjectileBlueprint,Barrel->GetSocketLocation(FName("Projectile")), Barrel->GetSocketRotation(FName("Projectile")));
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileBlueprint,Barrel->GetSocketLocation(FName("Projectile")), Barrel->GetSocketRotation(FName("Projectile")));
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
