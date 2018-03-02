f6ee8386d6e9e1ef6183bed1a77eb3f5ba8bef6a
poatir <poatir@gmail.com>
2018-03-02 01:23:58 -0500

    B162 Mejorando la arquitectura (moviendo forward) con thumbstick
    En la clase TankMovementComponent.h
    -haga forward de la clase:
    
    class UTankTrack;
    
    -Declare dos variables privadas asi:
    
    UTankTrack* LeftTrack = nullptr;
    UTankTrack* RightTrack = nullptr;
    
    Se modifica la ANOTACIÓN de la clase:
    
    UCLASS(ClassGroup=(Custom), meta = (BlueprintSpawnableComponent))
    
    se declara publicamente:
    
    UFUNCTION(BlueprintCallable, Category = Setup)
    void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
    
    En la clase TankMovementComponent.cpp
    void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
    {
    	if (!LeftTrackToSet || !RightTrackToSet) { return; }
    	LeftTrack = LeftTrackToSet;
    	RightTrack = RightTrackToSet;
    
    }
    
    En la clase Tank.cpp: en el constructor se comentarea la instruccion que sirve para crear DefaultSubObjeto:
    
    //TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
    Esta acción desaparece el componente del blueprint correspondiente a Tank_BP "Movement Component"
    
    La idea es compilar para que desaparezca y permita agregar un nuevo componente por menu de tipo:
    
    TankMovement. en la lista de subobjetos del tanque.
    -Presione adicionar Componente
    -Seleccione TankMovement.
    
    En Input Setup:
    -Arrastre una instancia de este componente
    y enlace con el nodo Intend Move Forward
    
    En la grafica de evento por defecto enlace Set Turret Reference pin exec con el nodo "initialise", que a su vez debe tener referenciado un objeto get TankMovement
    Ademas arrastre Left Track y Right track desde el arbol jerarquico del tanque y enlacelos a "Initialise"
    
    En la clase TankMovementComponent modifique el metodo IntendMoveForward agregando lo siguiente:
    Agrege en la cabezera:
    #Include "TankTrack.h"
    
    Apartir de ahora el tanque puede avanzar y retroceder con el stick izquierdo:
    No confundir con los otros botones, porque estos en realidad hacen rotar el tanque

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,10 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Intent move Forward: %f"), Throw);
+	//UE_LOG(LogTemp, Warning, TEXT("Intent move Forward: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+
 }
