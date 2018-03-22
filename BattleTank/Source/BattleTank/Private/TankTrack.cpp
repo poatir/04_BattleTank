c36b852c6fd98d7dcc1b030d01439f17d570d374
poatir <poatir@gmail.com>
2018-03-22 17:34:17 -0500

    B188 Eliminando fuerzas durante el vuelo parte 2
    Eliminar todos los tanque AI del nivel, para concentrarse en el movimiento del tanque.
    
    En Tank_BP seleccione Tank y en Collision Simulation Generate Hit events = true
    
    En Linear Damping establecer = 0.5 (amortiguación lineal)
    
    Amortiguación angular = 0.8
    
    Dentro de TankTrack.cpp:
    
    Seleccionar todo el codigo de TickComponent
    
    Boton derecho..Quick Actions->Extract
    
    La nueva funcion se llamara: ApplySidewaysForce()
    
    Mover todo al evento personalizado OnHit y eliminar TickComponent
    
    y PrimaryComponentTick.bCanEverTick = false;
    
    El DeltaTime que tenia el TickComponent se reemplaza en el codigo asi:
    
    void UTankTrack::ApplySidewaysForce()
    {
    	auto SlippageSpeed = FVector::CrossProduct(GetRightVector(), GetComponentVelocity());
    
    	auto DeltaTime = GetWorld()->GetDeltaSeconds();//Codigo Nuevo
    
    	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
    	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
    	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
    	TankRoot->AddForce(CorrectionForce);
    }
    
    En SetThrottle tomar todo su contenido y hacer un Extract Function a DriveTrack
    
    Quedando asi
    
    void UTankTrack::DriveTrack()
    {
    	auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
    	auto ForceLocation = GetComponentLocation();
    	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
    	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
    }
    
    void UTankTrack::SetThrottle()
    {
    	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
    }
    void DriveTrack();
    
    Declaramos en TankTrack.h: en private:
    
    float CurrentThrottle = 0;
    
    En OnHit agregar el metodo DriveTrack() asi:
    
    void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
    {
    	DriveTrack();
    	ApplySidewaysForce();
    	CurrentThrottle = 0;
    }
    
    Solo puede conducir y aplicar fuerzas si el tanque esta en el piso.
    
    Arrastrar 2 instancias del tanque...compilar

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,22 +2,11 @@
 #include "TankTrack.h"
 void UTankTrack::SetThrottle(float Throttle)
 {
-	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
-	auto ForceLocation = GetComponentLocation();
-	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
-	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
 }
 UTankTrack::UTankTrack()
 {
-	PrimaryComponentTick.bCanEverTick = true;
-}
-void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
-{
-	auto SlippageSpeed = FVector::CrossProduct(GetRightVector(), GetComponentVelocity());
-	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
-	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
-	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
-	TankRoot->AddForce(CorrectionForce);
+	PrimaryComponentTick.bCanEverTick = false;
 }
 void UTankTrack::BeginPlay()
 {
@@ -25,5 +14,23 @@ void UTankTrack::BeginPlay()
 }
 void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	UE_LOG(LogTemp,Warning,TEXT("Hit Hit hit"));
+	DriveTrack();
+	ApplySidewaysForce();
+	CurrentThrottle = 0;
+}
+void UTankTrack::ApplySidewaysForce()
+{
+	auto SlippageSpeed = FVector::CrossProduct(GetRightVector(), GetComponentVelocity());
+	auto DeltaTime = GetWorld()->GetDeltaSeconds();
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
+	TankRoot->AddForce(CorrectionForce);
+}
+void UTankTrack::DriveTrack()
+{
+	auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -12,8 +12,10 @@ public:
 	float TrackMaxDrivingForce = 400.000;
 private:
 	UTankTrack();
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction) override;
 	virtual void BeginPlay() override;
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	void ApplySidewaysForce();
+	void DriveTrack();
+	float CurrentThrottle = 0;
 };
